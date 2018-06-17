07e6364341e851ee11ae024f53cd0b37c2cc3c95
ilya-kondratyev <iljushka@list.ru>
2018-06-17 18:46:07 +0300

    Modified csv-io module

--- csv-io.py
+++ csv-io.py
@@ -1,19 +1,30 @@
 import csv
 
 
-def EMP_READ(filename='/Users/su/Documents/Ilya/staff-load-tests/Workbook1.csv'):
-    """Reads Employees infromation from file
-    returns dictionary of Employee records with Guid keys
+def emp_read(filename='/Users/su/Documents/Ilya/staff-load-tests/Workbook1.csv'):
     """
-    EmpDict=dict()
+    Reads Employees information from file
+    returns dictionary of Employee records with Guid keys
+
+    :param filename: name of the file containing Employees data in CSV format
+    :return: a dictionary of Employee records with Guid keys
+    """
+    empdict = dict()
     with open(filename, 'rU') as Emp_file:
-        Employeesd=csv.DictReader(Emp_file, dialect="excel")
-        for row in Employeesd:
-            EmpDict[row['Guid']]=row
-    return EmpDict
+        employeesd = csv.DictReader(Emp_file, dialect="excel")
+        for row in employeesd:
+            empdict[row['Guid']] = row
+    return empdict
 
 
-if __name__== "__main__":
-    EMP = EMP_READ()
+def emp_write(filename='/Users/su/Documents/Ilya/staff-load-tests/employees.csv'):
+    """
+
+    :param filename:
+    :return:
+    """
+
+if __name__ == "__main__":
+    EMP = emp_read()
     for i in EMP.keys():
         print EMP[i]
