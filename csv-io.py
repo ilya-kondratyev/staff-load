d9dae5317f5197fd37386b58f2ecb91a839014d3
ilya-kondratyev <iljushka@list.ru>
2018-06-17 17:00:10 +0300

    Added CSV-io module

--- csv-io.py
+++ csv-io.py
@@ -6,12 +6,9 @@ with open('/Users/su/Documents/Ilya/staff-load-tests/Workbook1.csv', 'rU') as Em
     Employeesd=csv.DictReader(Emp_file, dialect="excel")
     i=0
     for row in Employeesd:
-        print row.keys()
-        print row['Guid'] +' | ' + row['Name'] + ' | ' + row['Work Power']
-        EmpDict.
+        EmpDict[row['Guid']]=row
         i=i+1
-        print i
-        print '\n'
 
+print EmpDict
 
 
