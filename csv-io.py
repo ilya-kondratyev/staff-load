82478418bbb08d622679466e32ba3255b25f13b4
ilya-kondratyev <iljushka@list.ru>
2018-06-17 17:21:33 +0300

    Added EMP_READ function to CSV-io module

--- csv-io.py
+++ csv-io.py
@@ -1,14 +1,19 @@
 import csv
 
-EmpDict=dict()
-with open('/Users/su/Documents/Ilya/staff-load-tests/Workbook1.csv', 'rU') as Emp_file:
 
-    Employeesd=csv.DictReader(Emp_file, dialect="excel")
-    i=0
-    for row in Employeesd:
-        EmpDict[row['Guid']]=row
-        i=i+1
+def EMP_READ(filename='/Users/su/Documents/Ilya/staff-load-tests/Workbook1.csv'):
+    """Reads Employees infromation from file
+    returns dictionary of Employee records with Guid keys
+    """
+    EmpDict=dict()
+    with open(filename, 'rU') as Emp_file:
+        Employeesd=csv.DictReader(Emp_file, dialect="excel")
+        for row in Employeesd:
+            EmpDict[row['Guid']]=row
+    return EmpDict
 
-print EmpDict
 
-
+if __name__== "__main__":
+    EMP = EMP_READ()
+    for i in EMP.keys():
+        print EMP[i]
